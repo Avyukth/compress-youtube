765d373f38314db946e27f38feebd1c72148d638
Avyukth <makur.subhrajit@gmail.com>
2023-03-20 01:45:46 +0530

    add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
+extern crate flate2;
+use flate2::read::GzEncoder;
+use flate2::Compression;
+use std::env::args;
 use std::fs::File;
-use std::io::{Read, Write, BufReader, BufWriter};
-use flate2::{Compression, write::GzEncoder};
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
 
 fn main() {
-    let input_file = "input.txt";
-    let output_file = "compressed.gz";
-
-    if let Err(e) = compress_file(input_file, output_file) {
-        eprintln!("Error compressing file: {}", e);
-    } else {
-        println!("File compressed successfully.");
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        return;
     }
-}
-
-fn compress_file(input_path: &str, output_path: &str) -> std::io::Result<()> {
-    let input_file = File::open(input_path)?;
-    let mut reader = BufReader::new(input_file);
-
-    let output_file = File::create(output_path)?;
-    let mut writer = BufWriter::new(output_file);
-    let mut encoder = GzEncoder::new(writer, Compression::default());
-
-    let mut buffer = [0; 4096];
-    loop {
-        let read_bytes = reader.read(&mut buffer)?;
-        if read_bytes == 0 {
-            break;
-        }
-        encoder.write_all(&buffer[..read_bytes])?;
-    }
-
-    encoder.finish()?;
-    Ok(())
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+    println!("source len:{:?}", input.get_ref().metadata().unwrap().len());
+    println!("target len:{:?}", output.metadata().unwrap().len());
+    println!("time:{:?}", start.elapsed());
 }
